cdac4d83f50f555088b6c82677bd1b68843948e1
Shiv Bhonde <shivbhonde04@gmail.com>
2023-03-08 18:45:33 +0530

    complete template literals

--- src/03-template-literals/18-template-literals-in-object-keys.problem.ts
+++ src/03-template-literals/18-template-literals-in-object-keys.problem.ts
@@ -2,20 +2,20 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type TemplateLiteralKey = `${"user" | "post" | "comment"}${"Id" | "Name"}`;
 
-type ObjectOfKeys = unknown;
+type ObjectOfKeys = Record<TemplateLiteralKey, string>;
 
 type tests = [
-  Expect<
-    Equal<
-      ObjectOfKeys,
-      {
-        userId: string;
-        userName: string;
-        postId: string;
-        postName: string;
-        commentId: string;
-        commentName: string;
-      }
-    >
-  >,
+	Expect<
+		Equal<
+			ObjectOfKeys,
+			{
+				userId: string;
+				userName: string;
+				postId: string;
+				postName: string;
+				commentId: string;
+				commentName: string;
+			}
+		>
+	>
 ];
--- src/03-template-literals/19-uppercase-object.problem.ts
+++ src/03-template-literals/19-uppercase-object.problem.ts
@@ -2,17 +2,19 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Event = `log_in` | "log_out" | "sign_up";
 
-type ObjectOfKeys = unknown;
+type UppercaseEvent = Uppercase<Event>;
+
+type ObjectOfKeys = Record<UppercaseEvent, string>;
 
 type tests = [
-  Expect<
-    Equal<
-      ObjectOfKeys,
-      {
-        LOG_IN: string;
-        LOG_OUT: string;
-        SIGN_UP: string;
-      }
-    >
-  >,
+	Expect<
+		Equal<
+			ObjectOfKeys,
+			{
+				LOG_IN: string;
+				LOG_OUT: string;
+				SIGN_UP: string;
+			}
+		>
+	>
 ];
